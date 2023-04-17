0509f43108950b22cc36aeb2d5c9541db4ba72cd
codinghistorian <gaudihaus@gmail.com>
2023-04-17 15:09:39 +0400

    deployer as account0

--- scripts/common/addresses.js
+++ scripts/common/addresses.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const Deployer = "0x4C5F0f90a2D4b518aFba11E22AC9b8F6B031d204"
+const Deployer = "0x0Eb7DEE6e18Cce8fE839E986502d95d47dC0ADa3"
 
 const rawdata = fs.readFileSync('../../externalAddresses.json');
 const addresses = JSON.parse(rawdata);
