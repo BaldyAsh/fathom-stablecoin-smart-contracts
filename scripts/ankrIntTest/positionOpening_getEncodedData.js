9a982f8e22408f7f1b9ce7b54eafbf8a63a6be02
codinghistorian <gaudihaus@gmail.com>
2023-04-27 11:00:31 +0400

    2023 April 27th QA deployment test for mainnet, position opening data

--- scripts/ankrIntTest/positionOpening_getEncodedData.js
+++ scripts/ankrIntTest/positionOpening_getEncodedData.js
@@ -22,10 +22,10 @@ const openPositionAndDraw = async (collateral_pool_id, stablecoinAmount) => {
   ];
   const openLockTokenAndDrawIFace = new ethers.utils.Interface(openLockXDCAndDrawAbi);
   const openPositionCall = openLockTokenAndDrawIFace.encodeFunctionData("openLockXDCAndDraw", [
-      "0x7aD16A04cC612d2Ab1fDE424B214a3C36c49E61b", //Position Manager
-      "0x9E26db1dF46C77e8587B4859B8E8a0E0D036fd6C", // StabilityFeeCollector
-      "0x5eC92B5E8527d0C28282d230059E48093aaE9f82", //colTokenAdapter
-      "0xeDDe504B30b8231f8f6f50f4eB5f7b8B4136d18b", // StablecoinAdapter
+      "0x7A22e2386b41f542850938C92316fbDDe0Fe4D7d", //Position Manager
+      "0xc3fe9152F5d84daF850e2e7b8a0883DDf456641C", // StabilityFeeCollector
+      "0x01453d4a50e24471Fce64792A3B71BCbe7f73324", //colTokenAdapter
+      "0xdDD16D3dDa957Eb088c090e013F975B3fAAC05A2", // StablecoinAdapter
       collateral_pool_id,
       stablecoinAmount, // wad
       "0x00",
@@ -37,5 +37,5 @@ const openPositionAndDraw = async (collateral_pool_id, stablecoinAmount) => {
 }
 
 module.exports = async function(deployer) {
-  await openPositionAndDraw(COLLATERAL_POOL_ID, WeiPerWad.mul(1150000));
+  await openPositionAndDraw(COLLATERAL_POOL_ID, WeiPerWad.mul(100));
 };
