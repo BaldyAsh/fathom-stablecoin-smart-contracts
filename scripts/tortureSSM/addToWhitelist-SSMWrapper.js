f059d92d5844572edc965217a396a728576b661a
ssubik <shresthasubik@gmail.com>
2023-07-21 19:45:28 +0545

    modifying few stuff

--- scripts/tortureSSM/addToWhitelist-SSMWrapper.js
+++ scripts/tortureSSM/addToWhitelist-SSMWrapper.js
@@ -20,16 +20,12 @@ module.exports = async function(deployer) {
     const stableSwapWrapper = await artifacts.initializeInterfaceAt("StableSwapModuleWrapper", SSMWrapperAddress);
     
     console.log("Adding to whitelist -1");
+    await stableSwapWrapper.addToWhitelist('0x0Eb7DEE6e18Cce8fE839E986502d95d47dC0ADa3', { gasLimit: 1000000 })
+
+    console.log("Adding to whitelist -1");
     await stableSwapWrapper.addToWhitelist('0xE82C380C6Ca0306C61454569e84e020d68B063EF', { gasLimit: 1000000 })
     console.log("Adding to whitelist -1");
     await stableSwapWrapper.addToWhitelist('0x2B3691065A78F5fb02E9BF54A197b95da2B26AF7', { gasLimit: 1000000 })
     console.log("Adding to whitelist -1");
     await stableSwapWrapper.addToWhitelist('0xFa869165D4fB9DB1041eBc3E8D976847372FcF91', { gasLimit: 1000000 })
-    console.log("Adding to whitelist -1");
-    await stableSwapWrapper.addToWhitelist('0xd32Cd592c5296e893AfF7eb8518977A67e4b6741', { gasLimit: 1000000 })
-    console.log("Adding to whitelist -1");
-    await stableSwapWrapper.addToWhitelist('0xFE5247d790913A779CB3B24D7CD3da72e974a3D4', { gasLimit: 1000000 })
-    console.log("Adding to whitelist -1");
-    await stableSwapWrapper.addToWhitelist('0x6f342136EeafaEAF1b6056597f58060438946674', { gasLimit: 1000000 })
- 
 }
