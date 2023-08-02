1169c851881803da8a15e8d6c52b4e78f9158085
codinghistorian <gaudihaus@gmail.com>
2023-08-02 16:35:20 +0400

    openPositionEncodeForQA(emergency)env

--- scripts/encoding4Positions/openPosition.js
+++ scripts/encoding4Positions/openPosition.js
@@ -8,8 +8,6 @@ const { formatBytes32String } = require("ethers/lib/utils");
 
 const COLLATERAL_POOL_ID = formatBytes32String("XDC");
 
-const { AliceAddress } = require("../tests/helper/address");
-
 const { WeiPerWad } = require("../tests/helper/unit");
 
 
@@ -22,20 +20,19 @@ const openPositionAndDraw = async (collateral_pool_id, stablecoinAmount) => {
   ];
   const openLockTokenAndDrawIFace = new ethers.utils.Interface(openLockXDCAndDrawAbi);
   const openPositionCall = openLockTokenAndDrawIFace.encodeFunctionData("openLockXDCAndDraw", [
-      "0x9661dCD4043eDb266a43e4Bd65DCae87dE051FD6", //Position Manager
-      "0x2b7a0146E88fa7DC334d221561516f751ae3F8b9", // StabilityFeeCollector
-      "0x7793129dDB6de37249eF98168D989e5E2fBee76E", //AnkrCollateralAdapter
-      "0x2A63856eba3F3A1B07B6Cf3296D5e6f601E26044", // StablecoinAdapter
+      "0xCc42170fc2920d992BaE63f3a92003E61a322fc6", //Position Manager
+      "0x401989bb7F691523E92A349BeB02b1a832305f71", // StabilityFeeCollector
+      "0xa7d964773a1112c2C68a94575D7dba8F465efbA4", //AnkrCollateralAdapter
+      "0x40A3ecc81A3bC15Bd5A5c92d252BE4481f7f2902", // StablecoinAdapter
       collateral_pool_id,
       stablecoinAmount, // wad
       "0x00",
   ])
   console.log("below is the encoded data");
   console.log(openPositionCall);
-  // console.log("here2");
-  // await proxyWallet.execute2(fathomStablecoinProxyActions.address, openPositionCall, { from: from, value: ethers.constants.WeiPerEther })
+  console.log("here2");
 }
 
 module.exports = async function(deployer) {
-  await openPositionAndDraw(COLLATERAL_POOL_ID, WeiPerWad.mul(1150000));
+  await openPositionAndDraw(COLLATERAL_POOL_ID, WeiPerWad.mul(10));
 };
